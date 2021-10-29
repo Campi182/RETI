763071d1e01bdaa7ccd2881e8996ff4189c6ca94
Francesco Caneri <canerifrancesco@gmail.com>
2021-10-29 18:32:41 +0200

    Commit

--- Assignment6/src/Main.java
+++ Assignment6/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -8,10 +9,14 @@ public class Main {
 		
 		try(ServerSocket listener = new ServerSocket(6789)){ //in ascolto sulla porta 6789
 			System.out.println("Server is running...");
-			ExecutorService pool = Executors.newFixedThreadPool(10);
+			ExecutorService pool = Executors.newFixedThreadPool(10);	//possono essere gestite al max 10 connessione contemporaneamente
 			
 			while(true)
-				pool.execute(new TransferFile(listener.accept())); 
+				pool.execute(new TransferFile(listener.accept()));
+		} catch(IOException e) {
+			e.printStackTrace();
+			System.out.println("Server failure");
+			System.exit(-1);
 		}
 	}
 
--- Assignment6/src/TransferFile.java
+++ Assignment6/src/TransferFile.java
@@ -1,9 +1,9 @@
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -17,27 +17,66 @@ public class TransferFile implements Runnable{
 	public void run() {
 		System.out.println("Connected: "+socket);
 		
-		try(Scanner in = new Scanner(socket.getInputStream());
-			OutputStream out = socket.getOutputStream()
+		try(Scanner in = new Scanner(socket.getInputStream());	
+			OutputStream out = socket.getOutputStream();
 			){
-			String PathFile = in.nextLine().split(" ")[1].substring(1);
-			System.out.println("Requested: " + PathFile);
+			
+			String path = in.nextLine().split(" ")[1].substring(1);	//substring perchè tolgo il / all'inizio del path
+			String extension;
+			StringBuilder headers;
+			byte[] BytesFile = null;
+			
+			/*if(path.equals("HTTP")) {	//!!!!!NON VA BENE!!!!!!//
+				//redirect to qualcosa
+				System.out.println("Richiesta nulla");
+			} else System.out.println("Richiesta: "+path);*/
+			
+			
 			try {
-				File FileToSend = new File(PathFile);
-				byte[] fileByte = null;
-				fileByte = Files.readAllBytes(Paths.get(PathFile));
-				
-				//response
-				out.write(fileByte);
-				out.flush();
-				
-			} catch(Exception e) {
-				e.printStackTrace();
+			File RequestedFile = new File(path);
+			BytesFile = Files.readAllBytes(Paths.get(path));
+			} catch(NoSuchFileException e) {
+				System.out.println("404 File not found\n");
+				out.write(fileNotFoundMessage());
 			}
-		}//try
-		catch (IOException e) {
-
-			e.printStackTrace();
+			
+			int index = path.indexOf(".");
+			extension = path.substring(index);
+			System.out.println("Extension: "+extension);
+			
+			//!!!!INIZIALIZZAZIONE DEGLI HEADERS!!!!//
+			headers = new StringBuilder("HTTP/1.1 200 OK\n" + "Server: TransferFileServer\n");
+			
+			switch(extension) {
+			case ".txt":
+				headers.append("Content-Type: text/plain\n");
+				break;
+			case ".jpg":
+				
+				break;
+			case ".gif":
+				
+				break;
+			default:
+				break;
+			}
+				
+				
+			//aggiungo allo stream di uscita un 'a capo' per segnalare la fine degli headers
+			headers.append("\n");
+			
+			out.write(headers.toString().getBytes());
+			out.write(BytesFile);
+			out.flush();
+			
+		} catch(Exception e) {
+			System.out.println("Error "+ socket);
 		}
 	}//run
+	
+	//invoke when 404 File Not Found
+	private byte[] fileNotFoundMessage(){
+		String mes = "HTTP/1.1 404 Not Found\n"+"Server: TransferFileSever\n"+"Content-Type: text/html\n\n"+"<h1>ERROR 404&nbsp;</h1>\n"+"<p>Not Found</p>";
+		return mes.getBytes();
+	}
 }
